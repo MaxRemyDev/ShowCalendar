dcba0fe6f115f533b1996a9a306842244bd1b5d2
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-01-10 21:19:43 +0100

    added: initial structure

---
+++ Backend/Program.cs
@@ -0,0 +1,24 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.Run();
