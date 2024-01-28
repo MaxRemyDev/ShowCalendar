549437efc1d46be3f17264bfe5c482495df6566a
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-01-28 10:55:54 +0100

    added: data validation

--- Backend/Dtos/UserDto.cs
+++ Backend/Dtos/UserDto.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Backend.Dtos
 {
 
     public class UserRegistrationDto
     {
         public int UserId { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
         public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(1000, MinimumLength = 6)]
         public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
     }
 
