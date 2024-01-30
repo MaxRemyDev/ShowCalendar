a6923a011b05ac9bfd866f8565e78c295333a439
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-01-30 15:25:32 +0100

    replaced: exception to statuscode

--- Backend/Controllers/TestController.cs
+++ Backend/Controllers/TestController.cs
@@ -7,55 +7,39 @@ namespace Backend.Controllers
     [Route("api/test")]
     public class TestController : ControllerBase
     {
-        // GET: api/test
+        // GET: api/test/route => 200 OK RESPONSE
         [HttpGet("route")]
         public IActionResult Test()
         {
             return Ok("TEST: THIS IS A TEST ROUTE.");
         }
 
-        // GET: api/test/error
+        // GET: api/test/error => 500 INTERNAL SERVER ERROR RESPONSE
         [HttpGet("error")]
         public IActionResult CauseError()
         {
-            throw new Exception("TEST: THIS IS A SIMULATED ERROR TO TEST ERRORHANDLERMIDDLEWARE.");
+            return StatusCode(StatusCodes.Status500InternalServerError, "TEST: THIS IS A SIMULATED ERROR TO TEST ERRORHANDLERMIDDLEWARE.");
         }
 
-        // GET: api/test/notfound
+        // GET: api/test/notfound => 404 NOT FOUND RESPONSE
         [HttpGet("notfound")]
         public IActionResult CauseNotFoundError()
         {
-            throw new KeyNotFoundException("TEST: THE SPECIFIED KEY WAS NOT FOUND IN THE COLLECTION.");
+            return NotFound("TEST: THE SPECIFIED KEY WAS NOT FOUND IN THE COLLECTION.");
         }
 
-        // GET: api/test/unauthorized
+        // GET: api/test/unauthorized => 403 FORBIDDEN RESPONSE
         [HttpGet("unauthorized")]
         public IActionResult CauseUnauthorizedError()
         {
-            throw new UnauthorizedAccessException("TEST: THE USER DOES NOT HAVE PERMISSION TO PERFORM THIS ACTION.");
+            return StatusCode(StatusCodes.Status403Forbidden, "TEST: THE USER DOES NOT HAVE PERMISSION TO PERFORM THIS ACTION.");
         }
 
-        // GET: api/test/badrequest
+        // GET: api/test/badrequest => 400 BAD REQUEST RESPONSE
         [HttpGet("badrequest")]
         public IActionResult CauseBadRequestError()
         {
-            throw new ArgumentException("TEST: AN INVALID ARGUMENT WAS PROVIDED TO THE METHOD.");
-        }
-
-        // GET: api/test/customerror
-        [HttpGet("customerror")]
-        public IActionResult CauseCustomError()
-        {
-            throw new CustomException("TEST: THIS IS A CUSTOM EXCEPTION.");
-        }
-    }
-
-    // class CUSTOM EXCEPTION CLASS
-    public class CustomException : Exception
-    {
-        public CustomException(string message) : base(message)
-        {
-            // NEED CONFIGURATION TO HANDLE CUSTOM EXCEPTIONS
+            return BadRequest("TEST: AN INVALID ARGUMENT WAS PROVIDED TO THE METHOD.");
         }
     }
 }
