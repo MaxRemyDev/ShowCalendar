70da47154aa44bc0aee23a3eb9dc0d8e946b100e
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-01-30 15:22:42 +0100

    reorganized: mapping

--- Backend/Mappings/AutoMapperProfile.cs
+++ Backend/Mappings/AutoMapperProfile.cs
@@ -8,16 +8,23 @@ namespace Backend.Mappings
     {
         public AutoMapperProfile()
         {
-            CreateMap<CalendarDto, Calendar>().ReverseMap();
-            CreateMap<CalendarUpdateDto, Calendar>().ReverseMap();
-            CreateMap<AppointmentDto, Appointment>().ReverseMap();
-            CreateMap<AppointmentUpdateDto, Appointment>().ReverseMap();
-            CreateMap<UserDto, User>().ReverseMap();
-            CreateMap<UserUpdateDto, User>().ReverseMap();
-            CreateMap<NotificationDto, Notification>().ReverseMap();
-            CreateMap<NotificationUpdateDto, Notification>().ReverseMap();
+            // USER MAPPINGS
             CreateMap<UserRegistrationDto, User>().ReverseMap();
             CreateMap<UserLoginDto, User>().ReverseMap();
+            CreateMap<UserDto, User>().ReverseMap();
+            CreateMap<UserUpdateDto, User>().ReverseMap();
+
+            // CALENDAR MAPPINGS
+            CreateMap<CalendarDto, Calendar>().ReverseMap();
+            CreateMap<CalendarUpdateDto, Calendar>().ReverseMap();
+
+            // APPOINTMENT MAPPINGS
+            CreateMap<AppointmentDto, Appointment>().ReverseMap();
+            CreateMap<AppointmentUpdateDto, Appointment>().ReverseMap();
+
+            // NOTIFICATION MAPPINGS
+            CreateMap<NotificationDto, Notification>().ReverseMap();
+            CreateMap<NotificationUpdateDto, Notification>().ReverseMap();
         }
     }
 }
