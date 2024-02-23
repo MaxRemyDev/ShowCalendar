f1c47cf67845d584439550547cc8c38954e3a52a
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-02-23 01:20:27 +0100

    added: generic results helpers

---
+++ Backend/Helpers/Result.cs
@@ -0,0 +1,23 @@
+namespace Backend.Helpers
+{
+    // RESULT CLASS - GENERIC
+    public class Result<T>
+    {
+        // PROPERTIES - IS SUCCESS, VALUE, ERROR
+        public bool IsSuccess { get; }
+        public T? Value { get; }
+        public string? Error { get; }
+
+        // CONSTRUCTOR
+        protected Result(T? value, bool isSuccess, string? error)
+        {
+            Value = value;
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        // METHODS - SUCCESS, FAILURE
+        public static Result<T> Success(T value) => new Result<T>(value, true, null);
+        public static Result<T> Failure(string error) => new Result<T>(default(T), false, error);
+    }
+}
