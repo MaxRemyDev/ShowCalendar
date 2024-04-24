91e5db4a440cfea46722999e767b8c5be1ffd0a3
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-04-24 03:35:49 +0200

    fixed: import faqsection name

--- frontend/app/(landing)/home/page.tsx
+++ frontend/app/(landing)/home/page.tsx
@@ -1,7 +1,7 @@
 import { AppDownloadSection } from "@/features/landing/AppDownloadSection";
 import { BenefitsSection } from "@/features/landing/BenefitsSection";
 import { CompatibleToolsSection } from "@/features/landing/CompatibleToolsSection";
-import { FAQSection } from "@/features/landing/FaqSection";
+import { FAQSection } from "@/features/landing/FAQSection";
 import { FeaturesSection } from "@/features/landing/FeaturesSection";
 import { HeroSection } from "@/features/landing/HeroSection";
 import { LandingFooter } from "@/features/landing/LandingFooter";
@@ -9,18 +9,18 @@ import { LandingHeader } from "@/features/landing/LandingHeader";
 import { PricingSection } from "@/features/landing/PricingSection";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="h-16" />
-      <LandingHeader />
-      <HeroSection />
-      <FeaturesSection/>
-      <CompatibleToolsSection/>
-      <BenefitsSection/>
-      <AppDownloadSection/>
-      <PricingSection/>
-      <FAQSection/>
-      <LandingFooter/>
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-4">
+			<div className="h-16" />
+			<LandingHeader />
+			<HeroSection />
+			<FeaturesSection />
+			<CompatibleToolsSection />
+			<BenefitsSection />
+			<AppDownloadSection />
+			<PricingSection />
+			<FAQSection />
+			<LandingFooter />
+		</div>
+	);
 }
