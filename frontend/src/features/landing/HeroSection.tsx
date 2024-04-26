d25677f5bc4040b631dee8143e9f517aea3c868d
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-04-27 00:31:08 +0200

    added: image carousel, dots navigation and looping animation

--- frontend/src/features/landing/HeroSection.tsx
+++ frontend/src/features/landing/HeroSection.tsx
@@ -1,10 +1,73 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import { Section } from "./Section";
 import styles from "./modules/HeroSection.module.css";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect, useCallback } from "react";
 
 export const HeroSection = () => {
+	const images = [
+		"/assets/landing/hero/AppointmentsScreenshot.png",
+		"/assets/placeholder.svg",
+		"/assets/placeholder.svg",
+	];
+
+	const [[page, direction], setPage] = useState([0, 0]); // STATE FOR CURRENT PAGE AND DIRECTION
+
+	// BUTTON CLICK HANDLER
+	const paginate = useCallback(
+		(newDirection: number) => {
+			// COMPUTE NEW PAGE INDEX BASED ON DIRECTION
+			const imageIndex = (newDirection: number) => {
+				let newIndex = page + newDirection;
+				if (newIndex < 0) {
+					newIndex = images.length - 1;
+				} else if (newIndex >= images.length) {
+					newIndex = 0;
+				}
+				return newIndex;
+			};
+
+			// CAROUSEL IMAGE TRANSITION
+			const newIndex = imageIndex(newDirection);
+			setPage([newIndex, newDirection]);
+		},
+		[images.length, page] // DEPENDENCIES
+	);
+
+	// IMAGE CAROUSEL ANIMATION VARIANTS
+	const variants = {
+		enter: (direction: number) => ({
+			opacity: 0.4,
+			scale: 0.5,
+			x: direction > 0 ? 200 : -200,
+		}),
+		center: {
+			zIndex: 1,
+			opacity: 1,
+			scale: 1,
+			x: 0,
+		},
+		exit: (direction: number) => ({
+			zIndex: 0,
+			opacity: 0.1,
+			scale: 0.5,
+			x: direction > 0 ? -200 : 200,
+		}),
+	};
+
+	// CAROUSEL LOOP ANIMATION
+	useEffect(() => {
+		const interval = setInterval(() => {
+			paginate(1);
+		}, 7500); // MILLISECONDS (15 SECONDS)
+
+		return () => clearInterval(interval);
+	}, [page, paginate]);
+
 	return (
 		<Section>
 			<div className="flex flex-wrap items-center lg:flex-nowrap">
@@ -13,8 +76,11 @@ export const HeroSection = () => {
 					<div>
 						{/* TODO: REWORK ON LEADING_RELAXED RESPONSIVE */}
 						<h1
-							className={`text-4xl tracking-normal font-medium text-gray-900 md:text-5xl xl:text-6xl max-w-screen-md
-							leading-relaxed sm:leading-relaxed md:leading-relaxed lg:leading-relaxed xl:leading-relaxed 2xl:leading-relaxed ${styles.h1Responsive}`}
+							className={`
+								text-4xl tracking-normal font-medium text-gray-900 md:text-5xl xl:text-6xl max-w-screen-md
+								leading-relaxed sm:leading-relaxed md:leading-relaxed lg:leading-relaxed xl:leading-relaxed 2xl:leading-relaxed
+								${styles.h1Responsive}
+							`}
 						>
 							<span className="font-extrabold">All </span> Your Scheduling{" "}
 							<span className="font-extrabold relative inline-block" id="inOne">
@@ -57,15 +123,64 @@ export const HeroSection = () => {
 				</div>
 
 				{/* IMAGE SECTION */}
-				<div className="flex-1 flex lg:relative lg:w-1/2 max-w-screen-lg p-10 max-sm:p-5">
-					<Image
-						src="/assets/landing/hero/AppointmentsScreenshot.png"
-						alt="Appointments Screenshot"
-						width={700}
-						height={400}
-						style={{ objectFit: "contain" }}
-						priority
-					/>
+				<div className="flex-1 lg:relative lg:w-1/2 max-w-screen-lg py-5 px-5 max-sm:p-5">
+					{/* IMAGE CAROUSEL */}
+					<div className="relative h-[555px] flex flex-col justify-end">
+						<AnimatePresence initial={false} custom={direction}>
+							<motion.div
+								key={page}
+								custom={direction}
+								variants={variants}
+								initial="enter"
+								animate="center"
+								exit="exit"
+								transition={{
+									x: { type: "spring", stiffness: 300, damping: 30 },
+									opacity: { duration: 0.2 },
+									scale: { duration: 0.2 },
+								}}
+								drag="x"
+								dragConstraints={{ left: 0, right: 0 }}
+								dragElastic={1}
+								onDragEnd={(e: any, { offset, velocity }: any) => {
+									const swipeThreshold = 50;
+									if (Math.abs(offset.x) > swipeThreshold) {
+										const newDirection = offset.x > 0 ? -1 : 1;
+										paginate(newDirection);
+									}
+								}}
+								className="absolute inset-0 flex justify-center items-center cursor-grab"
+							>
+								<Image
+									src={images[page]}
+									alt="Hero Screenshot"
+									layout="fill"
+									objectFit="contain"
+									priority
+									onDragStart={(e) => e.preventDefault()}
+								/>
+							</motion.div>
+						</AnimatePresence>
+					</div>
+
+					{/* DOTS NAVIGATION CONTAINER */}
+					<div className="flex justify-center mt-5 z-10">
+						{images.map((_, index) => (
+							<motion.div
+								key={index}
+								onClick={() => setPage([index, 0])}
+								initial={{ opacity: 1 }}
+								animate={{ opacity: page === index ? 1 : 0.75 }}
+								className={`mx-2 cursor-pointer ${
+									page === index
+										? "h-4 w-12 bg-red-500 rounded-full"
+										: "h-4 w-4 bg-gray-300 rounded-full"
+								}`}
+								whileHover={{ scale: 1.25 }}
+								whileTap={{ scale: 0.9 }}
+							/>
+						))}
+					</div>
 				</div>
 			</div>
 		</Section>
