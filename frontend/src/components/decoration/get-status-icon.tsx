dc1f823b5a09409d516fc0d5b8635816a919a4a6
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-05-22 22:10:28 +0200

    added: new decoration icons for status indicators

--- frontend/src/components/decoration/get-status-icon.tsx
+++ frontend/src/components/decoration/get-status-icon.tsx
@@ -1,11 +1,40 @@
-import { Check, Hammer, Minus, X } from "lucide-react";
+import {
+	Check,
+	CircleCheck,
+	CircleCheckBig,
+	CircleMinus,
+	CircleX,
+	Construction,
+	Hammer,
+	Minus,
+	X,
+} from "lucide-react";
 
-const getStatusIcon = (decorationType: "check" | "line" | "x" | "work") => {
+const getStatusIcon = (
+	decorationType:
+		| "check"
+		| "circleCheck"
+		| "circleCheckBig"
+		| "line"
+		| "circleLine"
+		| "x"
+		| "circleX"
+		| "work"
+		| "work2"
+) => {
 	const decorations: { [key: string]: JSX.Element } = {
 		check: <Check className="text-greenColor" />,
+		circleCheck: <CircleCheck className="text-greenColor" />,
+		circleCheckBig: <CircleCheckBig className="text-greenColor" />,
+
 		line: <Minus className="text-orangeColor" />,
+		circleLine: <CircleMinus className="text-orangeColor" />,
+
 		x: <X className="text-redColor" />,
+		circleX: <CircleX className="text-redColor" />,
+
 		work: <Hammer className="text-orangeColor" />,
+		work2: <Construction className="text-orangeColor" />,
 	};
 	return decorations[decorationType] || null;
 };
