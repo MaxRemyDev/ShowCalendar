4402f1504acc757817230e32a7dbe874b805c816
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-05-27 04:15:13 +0200

    refactored: resizable panels to add sidebar navigation logic

--- frontend/src/features/dashboard/components/DashboardSection.tsx
+++ frontend/src/features/dashboard/components/DashboardSection.tsx
@@ -1,35 +1,74 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
 
 const DashboardSection = ({
 	sidebarContent,
 	content,
-	sidebarSize = 25,
-	contentSize = 75,
 	direction = "horizontal",
+	defaultLayout = [20, 80],
+	defaultCollapsed = false,
+	navCollapsedSize = 5,
 }: {
-	sidebarContent: React.ReactNode;
-	content: React.ReactNode;
-	sidebarSize?: number;
-	contentSize?: number;
+	sidebarContent?: React.ReactNode;
+	content?: React.ReactNode;
 	direction?: "horizontal" | "vertical";
+	defaultLayout?: number[] | undefined;
+	defaultCollapsed?: boolean;
+	navCollapsedSize?: number;
 }) => {
+	const [isCollapsed, setIsCollapsed] = useState(defaultCollapsed);
+	const [panelSize, setPanelSize] = useState<number>(defaultLayout[0]);
+
+	const handleCollapse = () => {
+		setIsCollapsed(!isCollapsed);
+		document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(!isCollapsed)}`;
+	};
+
+	//TODO: NEED MORE RESPONSIVE SIDEBAR COLLAPSED TO ADD LOGIC FOR DIFFERENT SCREEN SIZE TO COLLAPSE TO SAME LOCATION WITH PANELSIZE
+	//INFO: FOR EXAMPLE IF SIZE IS 15 ON "XL SCREEN", SIDEBAR WILL NORMALLY COLLAPSE TO RIGHT PLACE
+	//INFO: BUT IF SCREEN IS "MD", SIDEBAR IS SET TO 10, PANEL SIZE IT DOES NOT REDUCE TO SAME LOCATION AS ON “XL SCREEN”
+	useEffect(() => {
+		setIsCollapsed(panelSize < 10);
+		console.log(panelSize);
+	}, [panelSize]);
+
 	return (
-		<div className="dashboard-container">
-			<div className="main-content w-full h-screen">
-				<ResizablePanelGroup direction={direction}>
-					<ResizablePanel defaultSize={sidebarSize}>
-						<div className="flex h-full items-center justify-center p-6">
-							{sidebarContent}
-						</div>
+		<TooltipProvider delayDuration={0}>
+			<div className="main-content w-full h-full overflow-hidden">
+				<ResizablePanelGroup
+					direction={direction}
+					onLayout={(sizes: number[]) => {
+						document.cookie = `react-resizable-panels:layout=${JSON.stringify(sizes)}`;
+						setPanelSize(sizes[0]);
+					}}
+				>
+					<ResizablePanel
+						defaultSize={defaultLayout[0]}
+						collapsedSize={navCollapsedSize}
+						collapsible={true}
+						minSize={15}
+						maxSize={20}
+						onCollapse={handleCollapse}
+						onResize={(size: number) => setPanelSize(size)}
+						className={cn(
+							isCollapsed && "min-w-[50px] transition-all duration-300 ease-in-out"
+						)}
+					>
+						{React.cloneElement(sidebarContent as React.ReactElement<any>, {
+							isCollapsed,
+						})}
 					</ResizablePanel>
 					<ResizableHandle withHandle />
-					<ResizablePanel defaultSize={contentSize}>
+					<ResizablePanel defaultSize={defaultLayout[1]}>
 						<div className="flex h-full items-center justify-center p-6">{content}</div>
 					</ResizablePanel>
 				</ResizablePanelGroup>
 			</div>
-		</div>
+		</TooltipProvider>
 	);
 };
 
--- frontend/src/features/dashboard/pages/DashboardPage.tsx
+++ frontend/src/features/dashboard/pages/DashboardPage.tsx
@@ -1,24 +1,111 @@
+"use client";
+
 import React from "react";
 import DashboardSection from "../components/DashboardSection";
-import AppointmentList from "../components/AppointmentList";
-import CalendarView from "../components/CalendarView";
-import NotificationList from "../components/NotificationList";
+import { SidebarContent } from "../components/SidebarContent";
+import {
+	BarChartIcon,
+	CheckSquareIcon,
+	FileTextIcon,
+	HelpCircleIcon,
+	HomeIcon,
+	LogOutIcon,
+	MailIcon,
+	PieChartIcon,
+	SettingsIcon,
+	UsersIcon,
+} from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 const DashboardPage = () => {
 	return (
-		<div className="dashboard-container">
+		<div className="dashboard-container h-screen overflow-hidden">
 			<DashboardSection
-				sidebarContent={"Sidebar"}
-				content={
-					<div>
-						<CalendarView />
-						<AppointmentList />
-						<NotificationList />
-					</div>
+				sidebarContent={
+					<SidebarContent
+						isCollapsed={false}
+						links={[
+							{
+								title: "Dashboard",
+								href: "/dashboard",
+								label: "",
+								icon: HomeIcon,
+								variant: "default",
+							},
+							{
+								title: "Messages",
+								href: "/messages",
+								label: "128",
+								icon: MailIcon,
+								variant: "ghost",
+							},
+							{
+								title: "Tasks",
+								href: "/tasks",
+								label: "9",
+								icon: CheckSquareIcon,
+								variant: "ghost",
+							},
+							{
+								title: "Files",
+								href: "/files",
+								label: "",
+								icon: FileTextIcon,
+								variant: "ghost",
+							},
+							{
+								title: "Settings",
+								href: "/settings",
+								label: "",
+								icon: SettingsIcon,
+								variant: "ghost",
+								separator: (
+									<div className="py-5 ">
+										<Separator className="w-full" />
+									</div>
+								),
+							},
+							{
+								title: "Analytics",
+								href: "/analytics",
+								label: "972",
+								icon: BarChartIcon,
+								variant: "ghost",
+							},
+							{
+								title: "Contacts",
+								href: "/contacts",
+								label: "342",
+								icon: UsersIcon,
+								variant: "ghost",
+							},
+							{
+								title: "Reports",
+								href: "/reports",
+								label: "128",
+								icon: PieChartIcon,
+								variant: "ghost",
+							},
+							{
+								title: "Help",
+								href: "/help",
+								label: "8",
+								icon: HelpCircleIcon,
+								variant: "ghost",
+							},
+							{
+								title: "Logout",
+								href: "/logout",
+								label: "",
+								icon: LogOutIcon,
+								variant: "ghost",
+							},
+						]}
+					/>
 				}
-				sidebarSize={25}
-				contentSize={75}
+				content={<div>Content</div>}
 				direction="horizontal"
+				defaultCollapsed={false}
 			/>
 		</div>
 	);
