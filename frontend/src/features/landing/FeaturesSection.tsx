9060e450880da89cee4e78d01e2a382302ed1d98
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-05-31 09:01:10 +0200

    fixed: resolve type error for element assignment in ref callback

--- frontend/src/features/landing/FeaturesSection.tsx
+++ frontend/src/features/landing/FeaturesSection.tsx
@@ -168,9 +168,11 @@ export const FeaturesSection = () => {
 													style={{
 														filter: "drop-shadow(0px 0px 25px rgba(0, 0, 0, 0.25))",
 													}}
-													ref={(el: HTMLDivElement | null) =>
-														(cardRefs.current[cardIndex] = el)
-													}
+													ref={(el) => {
+														if (el) {
+															cardRefs.current[cardIndex] = el;
+														}
+													}}
 													// MOUSE INTERACTIONS FOR 3D EFFECT
 													onMouseMove={handleMouseMove(cardIndex)}
 													onMouseLeave={() => handleMoveLeave(cardIndex)}
