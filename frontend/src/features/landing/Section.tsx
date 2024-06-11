f965311c861352bfb38a8bda8a44befec6931727
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-06-11 18:29:08 +0200

    added: ease in out animation for each section that is displayed with framer motion

--- frontend/src/features/landing/Section.tsx
+++ frontend/src/features/landing/Section.tsx
@@ -4,18 +4,79 @@
 	INFO: THUS FACILITATING CODE REUSE AND DESIGN UNIFORMITY ACROSS PAGE.
 */
 
+"use client";
+
 import { cn } from "@/lib/utils";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useRef, useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 export type SectionProps = PropsWithChildren<{
 	className?: string;
 	sectionClassName?: string;
 	id?: string;
+	disableAnimations?: boolean;
 }>;
 
 export const Section = (props: SectionProps) => {
+	const ref = useRef(null);
+	const [isInView, setIsInView] = useState(false);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						setIsInView(true);
+					} else {
+						setIsInView(false);
+					}
+				});
+			},
+			{ threshold: 0.1 }
+		);
+		if (ref.current) {
+			observer.observe(ref.current);
+		}
+		return () => {
+			if (ref.current) {
+				observer.unobserve(ref.current);
+			}
+		};
+	}, [ref]);
+
+	const variants = {
+		hidden: { opacity: 0, y: 50, scale: 0.9 },
+		visible: { opacity: 1, y: 0, scale: 1 },
+	};
+
+	if (props.disableAnimations) {
+		return (
+			<section id={props.id} className={cn(props.sectionClassName)} ref={ref}>
+				<div
+					className={cn(
+						"flex flex-col justify-center px-20 py-10 border-solid border-b-[3px] border-background-200 dark:border-background-100 max-md:px-5 max-md:py-5",
+						props.className
+					)}
+				>
+					{props.children}
+				</div>
+			</section>
+		);
+	}
+
 	return (
-		<section id={props.id} className={cn(props.sectionClassName)}>
+		<motion.section
+			id={props.id}
+			className={cn(props.sectionClassName)}
+			ref={ref}
+			initial="hidden"
+			animate={isInView ? "visible" : "hidden"}
+			variants={variants}
+			transition={{
+				duration: 0.8,
+				ease: "easeInOut",
+			}}
+		>
 			<div
 				className={cn(
 					"flex flex-col justify-center px-20 py-10 border-solid border-b-[3px] border-background-200 dark:border-background-100 max-md:px-5 max-md:py-5",
@@ -24,6 +85,6 @@ export const Section = (props: SectionProps) => {
 			>
 				{props.children}
 			</div>
-		</section>
+		</motion.section>
 	);
 };
