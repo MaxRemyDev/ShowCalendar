c88c2e5ac6c3f20a34315a4ec4822d4dbda41ece
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-06-30 03:36:58 +0200

    updated: to integrate striped pattern for weekend background decoration

--- frontend/src/features/dashboard/pages/contents/calendar/components/CalendarGrid.tsx
+++ frontend/src/features/dashboard/pages/contents/calendar/components/CalendarGrid.tsx
@@ -14,6 +14,7 @@ import PastDateIndicator from "./ui/past-date-indicator";
 import { Button } from "@/components/ui/button";
 import DragAndDropEvent from "./ui/drag-and-drop-event";
 import { cn } from "@/lib/utils";
+import StripedPattern from "@/components/decoration/striped-pattern";
 
 interface CalendarGridProps {
 	currentDate: Date;
@@ -32,6 +33,11 @@ interface CalendarGridProps {
 	startDate: Date;
 	showEventModal: (date: Date) => void;
 }
+
+const isWeekend = (date: Date) => {
+	const day = date.getDay();
+	return day === 0 || day === 6;
+};
 
 const CalendarGrid: React.FC<CalendarGridProps> = ({
 	currentDate,
@@ -63,170 +69,109 @@ const CalendarGrid: React.FC<CalendarGridProps> = ({
 		return eachDayOfInterval({ start: startDate, end: endDate });
 	};
 
-	const renderDays = () => {
-		if (showConsecutiveDays) {
-			return getConsecutiveDaysArray().map((date, index) => {
-				const isEndOfMonth = isLastDayOfMonth(date);
-				return (
-					<div
-						key={index}
-						style={{ width: "14.28%", height: "120px" }}
-						className={cn("px-4 pt-2 relative border-2 rounded-lg shadow-sm", {
-							"border-dashed border-primary":
-								selectedDay && isSameDay(date, selectedDay) && !isDragging,
-							"border-dashed border-secondary":
-								isDragging && hoveredDay && isSameDay(date, hoveredDay),
-							"border-dashed border-background-700":
-								hoveredDay &&
-								isSameDay(date, hoveredDay) &&
-								!isDragging &&
-								!(selectedDay && isSameDay(date, selectedDay)),
-						})}
-						onMouseOver={() => onDayHover(date)}
-						onDragOver={() => onDayHover(date)}
-						onFocus={() => onDayHover(date)}
-						onClick={() => onDayClick(date)}
-						tabIndex={0}
-						role="button"
-						aria-pressed={selectedDay ? "true" : "false"}
-					>
-						<PastDateIndicator date={date} />
-						<Button
-							onClick={() => showEventModal(date)}
-							onKeyDown={(e) => e.key === "Enter" && showEventModal(date)}
-							variant="ghost"
-							disabled={isPastDate(date)}
-							className={cn(
-								"inline-flex items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 w-8 h-8",
-								{
-									"bg-primary hover:bg-secondary text-primary-foreground":
-										isToday(date) && !isEndOfMonth,
-									"-ml-3 w-16 bg-background-200 dark:bg-background-100":
-										isEndOfMonth && !isToday(date),
-									"-ml-3 w-16 bg-primary hover:bg-secondary text-primary-foreground":
-										isToday(date) && isEndOfMonth,
-								}
-							)}
-							aria-pressed={selectedDay ? "true" : "false"}
-						>
-							{date.getDate()}
-							{isEndOfMonth && <div className="ml-1">{format(date, "MMM")}</div>}
-						</Button>
-						<DragAndDropEvent
-							date={date.getDate()}
-							month={date.getMonth()}
-							year={date.getFullYear()}
-							events={events.filter((event) =>
-								isSameDay(new Date(event.event_date), date)
-							)}
-							updateEventPosition={updateEventPosition}
-							onDayHover={onDayHover}
-							onDayClick={onDayClick}
-							handleDragEnd={handleDragEnd}
-							selectedDay={selectedDay}
-							hoveredDay={hoveredDay}
-							setIsDragging={setIsDragging}
-							handleDragStart={handleDragStart}
-							isDragging={isDragging}
-							setSelectedDay={setSelectedDay}
-							showConsecutiveDays={showConsecutiveDays}
-							startDate={currentDate}
-							showEventModal={showEventModal}
-						/>
-					</div>
-				);
-			});
-		} else {
-			return daysArray.map((date, index) => (
-				<div
-					key={index}
-					style={{ width: "14.28%", height: "120px" }}
-					className={cn("px-4 pt-2 relative border-2 rounded-lg shadow-sm", {
-						"border-dashed border-primary":
-							selectedDay && isSameDay(date, selectedDay) && !isDragging,
-						"border-dashed border-secondary":
-							isDragging && hoveredDay && isSameDay(date, hoveredDay),
-						"border-dashed border-background-700":
-							hoveredDay &&
-							isSameDay(date, hoveredDay) &&
-							!isDragging &&
-							!(selectedDay && isSameDay(date, selectedDay)),
-					})}
-					onMouseOver={() => onDayHover(date)}
-					onDragOver={() => onDayHover(date)}
-					onFocus={() => onDayHover(date)}
-					onClick={() => onDayClick(date)}
-					tabIndex={0}
-					role="button"
+	const renderDayContent = (date: Date) => {
+		const isEndOfMonth = isLastDayOfMonth(date);
+		return (
+			<div
+				style={{ width: "100%", height: "100%" }}
+				className={cn("px-4 pt-2 relative border-2 rounded-lg shadow-sm", {
+					"border-dashed border-primary":
+						selectedDay && isSameDay(date, selectedDay) && !isDragging,
+					"border-dashed border-secondary":
+						isDragging && hoveredDay && isSameDay(date, hoveredDay),
+					"border-dashed border-background-700":
+						hoveredDay &&
+						isSameDay(date, hoveredDay) &&
+						!isDragging &&
+						!(selectedDay && isSameDay(date, selectedDay)),
+				})}
+				onMouseOver={() => onDayHover(date)}
+				onDragOver={() => onDayHover(date)}
+				onFocus={() => onDayHover(date)}
+				onClick={() => onDayClick(date)}
+				tabIndex={0}
+				role="button"
+				aria-pressed={selectedDay ? "true" : "false"}
+			>
+				<PastDateIndicator date={date} />
+				<Button
+					onClick={() => showEventModal(date)}
+					onKeyDown={(e) => e.key === "Enter" && showEventModal(date)}
+					variant="ghost"
+					disabled={isPastDate(date)}
+					className={cn(
+						"inline-flex items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 w-8 h-8",
+						{
+							"bg-primary hover:bg-secondary text-primary-foreground":
+								isToday(date) && !isEndOfMonth,
+							"-ml-3 w-16 border-4 border-background-200":
+								isEndOfMonth && !isToday(date) && showConsecutiveDays,
+							"-ml-3 w-16 bg-primary hover:bg-secondary text-primary-foreground":
+								isToday(date) && isEndOfMonth,
+						}
+					)}
 					aria-pressed={selectedDay ? "true" : "false"}
 				>
-					<PastDateIndicator date={date} />
-					<Button
-						onClick={() => showEventModal(date)}
-						onKeyDown={(e) => e.key === "Enter" && showEventModal(date)}
-						variant="ghost"
-						disabled={isPastDate(date)}
-						className={cn(
-							"inline-flex items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 w-8 h-8",
-							{
-								"bg-primary hover:bg-secondary text-primary-foreground":
-									isToday(date) && !isLastDayOfMonth(date),
-								"-ml-3 w-16 bg-background-200 dark:bg-background-100":
-									isLastDayOfMonth(date) && !isToday(date),
-								"-ml-3 w-16 bg-primary hover:bg-secondary text-primary-foreground":
-									isToday(date) && isLastDayOfMonth(date),
-							}
-						)}
-						aria-pressed={selectedDay ? "true" : "false"}
-					>
-						{date.getDate()}
-						{isLastDayOfMonth(date) && (
-							<div className="ml-1">{format(date, "MMM")}</div>
-						)}
-					</Button>
-					<DragAndDropEvent
-						date={date.getDate()}
-						month={month}
-						year={year}
-						events={events.filter((event) =>
-							isSameDay(new Date(event.event_date), date)
-						)}
-						updateEventPosition={updateEventPosition}
-						onDayHover={onDayHover}
-						onDayClick={onDayClick}
-						handleDragEnd={handleDragEnd}
-						selectedDay={selectedDay}
-						hoveredDay={hoveredDay}
-						setIsDragging={setIsDragging}
-						handleDragStart={handleDragStart}
-						isDragging={isDragging}
-						setSelectedDay={setSelectedDay}
-						showConsecutiveDays={showConsecutiveDays}
-						startDate={currentDate}
-						showEventModal={showEventModal}
-					/>
+					{date.getDate()}
+					{isEndOfMonth && showConsecutiveDays && (
+						<div className="ml-1">{format(date, "MMM")}</div>
+					)}
+				</Button>
+				<DragAndDropEvent
+					date={date.getDate()}
+					month={date.getMonth()}
+					year={date.getFullYear()}
+					events={events.filter((event) => isSameDay(new Date(event.event_date), date))}
+					updateEventPosition={updateEventPosition}
+					onDayHover={onDayHover}
+					onDayClick={onDayClick}
+					handleDragEnd={handleDragEnd}
+					selectedDay={selectedDay}
+					hoveredDay={hoveredDay}
+					setIsDragging={setIsDragging}
+					handleDragStart={handleDragStart}
+					isDragging={isDragging}
+					setSelectedDay={setSelectedDay}
+					showConsecutiveDays={showConsecutiveDays}
+					startDate={currentDate}
+					showEventModal={showEventModal}
+				/>
+			</div>
+		);
+	};
+
+	const renderDays = () => {
+		const days = showConsecutiveDays ? getConsecutiveDaysArray() : daysArray;
+		return days.map((date, index) => {
+			const dayContent = renderDayContent(date);
+			return (
+				<div key={index} className="w-[14.28%] h-[120px]">
+					{isWeekend(date) ? (
+						<StripedPattern className="w-full h-full">{dayContent}</StripedPattern>
+					) : (
+						dayContent
+					)}
 				</div>
-			));
-		}
+			);
+		});
 	};
 
 	return (
-		<div id="calendar-grid" className="-mx-1 -mb-1">
-			<div className="flex flex-wrap" style={{ marginBottom: -40 }}>
+		<div id="calendar-grid" className="flex flex-wrap -mx-1 -mb-1">
+			<div className="flex flex-wrap w-full mb-8">
 				{DAYS_NAMES.map((day) => (
-					<div key={day} style={{ width: "14.26%" }} className="px-2 py-2">
+					<div key={day} className="w-[14.28%] px-2 py-2">
 						<div className="text-foreground-600 text-sm uppercase tracking-wide font-bold text-center">
 							{day}
 						</div>
 					</div>
 				))}
 			</div>
-			<div className="flex flex-wrap">
+			<div className="flex flex-wrap w-full">
 				{getBlankDaysInMonth(year, month).map((_, index) => (
 					<div
 						key={index}
-						style={{ width: "14.28%", height: "120px" }}
-						className="text-center border-r border-b px-4 pt-2"
+						className="w-[14.28%] h-[120px] text-center border-r border-b px-4 pt-2"
 					/>
 				))}
 				{renderDays()}
