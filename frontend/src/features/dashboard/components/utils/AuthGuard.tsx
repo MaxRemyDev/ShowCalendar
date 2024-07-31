9a0e07247f40eb73768fbdf28406f948e07a4832
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-07-31 15:21:53 +0200

    updated: authentication and session management
    - AuthenticatedRoute.tsx
    	added: token expiration check to ensure valid sessions
    	implemented: periodic authentication checks and session expiry handling
    	integrated: toast notification for session expiration
    	refactored: logic to handle user logout and redirect to login page
    
    - AuthGuard.tsx
    	added: token expiration validation and automatic session management
    	implemented: periodic checks for authentication status
    	refactored: session expiration handling with user logout and redirect

--- frontend/src/features/dashboard/components/utils/AuthGuard.tsx
+++ frontend/src/features/dashboard/components/utils/AuthGuard.tsx
@@ -4,7 +4,9 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useUser } from "@/features/dashboard/components/utils/UserContext";
 import Spinner from "@/components/decoration/spinner";
-import { getCookie } from "cookies-next";
+import { getCookie, deleteCookie } from "cookies-next";
+
+let hasLoggedOut = false;
 
 const AuthGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 	const { user, loading, refreshUser } = useUser();
@@ -13,8 +15,15 @@ const AuthGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
 	useEffect(() => {
 		const checkAuth = async () => {
+			if (hasLoggedOut) return;
+
 			const token = getCookie("token");
 			if (token) {
+				const tokenExpired = isTokenExpired(token);
+				if (tokenExpired) {
+					await handleTokenExpired(router);
+					return;
+				}
 				const refreshedUser = await refreshUser();
 				if (refreshedUser) {
 					router.replace("/dashboard");
@@ -26,6 +35,10 @@ const AuthGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 		if (!loading && !authChecked) {
 			checkAuth();
 		}
+
+		const intervalId = setInterval(checkAuth, 60000);
+
+		return () => clearInterval(intervalId);
 	}, [user, loading, router, refreshUser, authChecked]);
 
 	if (loading || !authChecked) {
@@ -38,5 +51,24 @@ const AuthGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
 	return <>{children}</>;
 };
+
+const isTokenExpired = (token: string) => {
+	const decodedToken = JSON.parse(atob(token.split(".")[1]));
+	const expiry = decodedToken.exp;
+	const now = Math.floor(Date.now() / 1000);
+	return now > expiry;
+};
+
+const handleTokenExpired = async (router: ReturnType<typeof useRouter>) => {
+	if (hasLoggedOut) return;
+
+	hasLoggedOut = true;
+	deleteCookie("token");
+	deleteCookie("refreshToken");
+	deleteCookie("userId");
+	router.push("/login");
+};
+
+AuthGuard.displayName = "AuthGuard";
 
 export default AuthGuard;
--- frontend/src/features/dashboard/components/utils/AuthenticatedRoute.tsx
+++ frontend/src/features/dashboard/components/utils/AuthenticatedRoute.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useUser } from "@/features/dashboard/components/utils/UserContext";
-import { useRouter } from "next/navigation";
 import Spinner from "@/components/decoration/spinner";
-import { getCookie } from "cookies-next";
+import { getCookie, deleteCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
+import { toast } from "@/components/ui/use-toast";
+
+let hasLoggedOut = false;
 
 const AuthenticatedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 	const { user, loading, refreshUser } = useUser();
@@ -11,16 +14,22 @@ const AuthenticatedRoute: React.FC<{ children: React.ReactNode }> = ({ children 
 
 	useEffect(() => {
 		const checkAuth = async () => {
+			if (hasLoggedOut) return;
+
 			const token = getCookie("token");
-			console.log("AuthenticatedRoute: Token from cookie:", token);
 			if (!token) {
-				router.push("/login");
+				await handleTokenExpired(router);
+				return;
+			}
+			const tokenExpired = isTokenExpired(token);
+			if (tokenExpired) {
+				await handleTokenExpired(router);
 				return;
 			}
 			if (!user) {
 				const refreshedUser = await refreshUser();
 				if (!refreshedUser) {
-					router.push("/login");
+					await handleTokenExpired(router);
 					return;
 				}
 			}
@@ -30,6 +39,10 @@ const AuthenticatedRoute: React.FC<{ children: React.ReactNode }> = ({ children 
 		if (!loading && !authChecked) {
 			checkAuth();
 		}
+
+		const intervalId = setInterval(checkAuth, 60000); // 60 SECONDS
+
+		return () => clearInterval(intervalId);
 	}, [user, loading, router, refreshUser, authChecked]);
 
 	if (loading || !authChecked) {
@@ -42,5 +55,29 @@ const AuthenticatedRoute: React.FC<{ children: React.ReactNode }> = ({ children 
 
 	return <>{children}</>;
 };
+
+const isTokenExpired = (token: string) => {
+	const decodedToken = JSON.parse(atob(token.split(".")[1]));
+	const expiry = decodedToken.exp;
+	const now = Math.floor(Date.now() / 1000);
+	return now > expiry;
+};
+
+const handleTokenExpired = async (router: ReturnType<typeof useRouter>) => {
+	if (hasLoggedOut) return;
+
+	hasLoggedOut = true;
+	deleteCookie("token");
+	deleteCookie("refreshToken");
+	deleteCookie("userId");
+	toast({
+		title: "Session Expired",
+		description: "Your session has expired. Please log in again.",
+		variant: "destructive",
+	});
+	router.push("/login");
+};
+
+AuthenticatedRoute.displayName = "AuthenticatedRoute";
 
 export default AuthenticatedRoute;
